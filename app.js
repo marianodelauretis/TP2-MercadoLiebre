638a6a660dfcf333c54efd9bd2126e4a02d17658
marianodelauretis@gmail.com <marianodelauretis@gmail.com>
2023-07-25 20:07:10 -0300

    Corrección listen

---
+++ app.js
@@ -0,0 +1,12 @@
+const express =require("express")
+const app =express ()
+const path =require ("path")
+app.use(express.static("public"))
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+  })
+
+app.get ("/",function(req , res){
+    res.sendFile(path.join(__dirname,"/views/home.html"))
+})
